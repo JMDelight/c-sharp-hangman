1d464416294235825b89e670848bd9c0e0996a27
Joel Delight and Shradha Pulla <joel+shradha@>
2016-06-30 09:12:35 -0700

    Create basic files, create index.cshtml

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using Hangman.Objects;
+using System.Collections.Generic;
+
+namespace Hangman
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+
+    }
+  }
+}
